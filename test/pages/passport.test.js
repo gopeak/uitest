7ab740aa4fae53ff28586eb88233a7776bfe039f
赵龙 <mnoxao@vip.qq.com>
2020-12-03 21:02:11 +0800

    add

--- test/pages/passport.test.js
+++ test/pages/passport.test.js
@@ -1,6 +1,7 @@
 import faker from 'faker';
 import puppeteer from 'puppeteer';
 
+
 import routes from '../routes';
 import $browser from '../config/browser';
 import $config from '../config/index';
@@ -14,26 +15,40 @@ beforeAll(async () => {
     page = await browser.newPage()
 })
 
+let defaultUsername = $config.page.username;
+let defaultPassword = $config.page.password;
 
+let $signInUsername = "#user_login";
+let $signInPassword = "#user_password";
+let $signInSubmit = "#login_submit_btn";
+let $signInRemember = "#user_remember_me";
 
+// 登录
+const signIn = async remember => {
+    await page.goto(routes.index)
+    await page.type($signInUsername, defaultUsername);
+    await page.type($signInPassword, defaultPassword);
+    if (remember) {
+        await page.click($signInRemember);
+    }
+    await page.click($signInSubmit);
+}
 
-describe('首页', () => {
-    test('#PASSPORT-1-入口页面', async () => {
-        await page.goto(routes.index)
-        await page.waitForSelector('#login_submit_btn')
-        await page.waitForSelector('#user_login')
-        await page.waitForSelector('#user_password')
-        await browser.close();
-    })
-})
-
-describe('注册及找回密码', () => {
+describe('入口', () => {
     let regInfo = {
         email: faker.internet.email(),
         password: faker.internet.password(),
         display_name: faker.name.findName(),
         username: faker.name.firstName()
     }
+    test('#PASSPORT-1-入口页面', async () => {
+        await page.goto(routes.index)
+        await page.waitForSelector($signInSubmit)
+        await page.waitForSelector($signInUsername)
+        await page.waitForSelector($signInPassword)
+        await browser.close();
+    })
+
     test('#PASSPORT-2-注册', async () => {
         await page.goto(routes.index)
         await page.click('#li-register');
@@ -43,15 +58,38 @@ describe('注册及找回密码', () => {
         await page.type('#new_user_email_confirmation', regInfo.email);
         await page.type('#new_user_password', regInfo.password);
         await page.click("#register_submit_btn");
-        await page.waitForNavigation()
+        await page.waitForSelector('.user-avatar')
+        await page.close();
     })
+
     test('#PASSPORT-3-找回密码', async () => {
-        await page.click('.forgot-password');
-        await page.type('#user_email', regInfo.email);
-        await page.click("#commit");
-        let res = await page.evaluate(res => {
-            console.log(12345)
-            return res
+
+    })
+
+    test('#PASSPORT-4-记住密码', async () => {
+        let newPage = null;
+        await signIn(true)
+        await page.waitForSelector('.user-avatar')
+        await page.close();
+        newPage = await browser.newPage()
+        await newPage.goto(routes.index)
+        await newPage.waitForSelector('.user-avatar')
+        await browser.close();
+    })
+})
+
+
+describe('首页', () => {
+    test('#INDEX-1-首页', async () => {
+        let $nums = ".extra-item-num";
+        await signIn()
+        await page.waitForSelector('.user-avatar')
+        const nums = await page.$$eval($nums, items => {
+            return items.map(item => Number(item.innerText))
         })
+        for (let num of nums) {
+            expect(num).toBeGreaterThanOrEqual(0)
+        }
+        await browser.close();
     })
 })
